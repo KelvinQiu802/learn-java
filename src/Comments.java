012461e999f62574a0700da7e3cacdad9999634d
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-23 08:27:56 +0800

    feat: init

---
+++ src/Comments.java
@@ -0,0 +1,21 @@
+public class Comments {
+    // First type of comments
+
+    /*
+    Second type of comments
+    Second type of comments
+    Second type of comments
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * This is a comment used to generate java doc.
+     * @author KelvinQiu
+     */
+    public static void speak() {
+        System.out.println("Speaking");
+    }
+}
